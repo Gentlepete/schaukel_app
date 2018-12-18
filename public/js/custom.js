aeae67029ecd8bf6d884db4f45ca66c9e4dea228
Patrick <pas@click-solutions.de>
2018-12-18 19:48:34 +0100

    change function to real values
    check if values have changed, and then start recording differences and show cam after 100 similar values

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,10 @@
-function showCam(video) {
+let movedOne = false;
+let movedTwo = false;
+let chartOneValue = 0;
+let chartTwoValue = 0;
+let equalCounter = 0;
+
+function showCam() {
     $("canvas").addClass("hide");
     $("video").addClass("show");
 }
@@ -6,6 +12,8 @@ function showCam(video) {
 function hideCam() {
     $("canvas").removeClass("hide");
     $("video").removeClass("show");
+    movedOne = false;
+    movedTwo = false;
 }
 
 $(document).ready(function(){
@@ -25,14 +33,12 @@ $(document).ready(function(){
             video.srcObject = stream;
         })
         .catch(function(error) {
-            console.log("Somethin went wrong");
+            //console.log("Somethin went wrong");
         });
-
     }
 
-
-    var ctx = document.getElementById('frequency-chart');
-    var myLineChart = new Chart(ctx, {
+    let ctx = document.getElementById('frequency-chart');
+    let myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: "",
@@ -83,12 +89,16 @@ $(document).ready(function(){
         }
     });
         
-    var socket = io();
+    let socket = io();
 
-    let chartOneValue = 0;
     socket.on('sendData', function(message) {
         message = Math.round(message / 10) * 10;
         chartOneValue = message;
+
+        if(message > 600 || message < 400) {
+            movedOne = true;
+        }
+
         myLineChart.data.labels.push("");
         myLineChart.data.datasets[0].data.push(message);
 
@@ -101,36 +111,38 @@ $(document).ready(function(){
         }  
     });
 
-    let chartTwoValue = 0;
     socket.on('sendData2', function(message) {
         message = Math.round(message / 10) * 10;
         chartTwoValue = message;
+
+        if(message > 600 || message < 400) {
+            movedTwo = true;
+        }
+
         myLineChart.data.datasets[1].data.push(message);
 
         myLineChart.update(10);
 
         if(myLineChart.data.datasets[0].data.length > 38) {
             myLineChart.data.datasets[1].data.shift();
-            // myLineChart.data.labels.shift();
         }
     });
 
-    var equalCounter = 0;
-
     setInterval(function(){
-        // Check if chart values are smiliar
-        var difference = Math.abs(chartOneValue - chartTwoValue); 
+        if (movedOne || movedTwo) {
+            // Check if chart values are smiliar
+            var difference = Math.abs(chartOneValue - chartTwoValue); 
 
-        if (difference <= 100) {
-            // console.log(difference);
-            equalCounter++;
-        } else {
-            equalCounter = 0;
+            if (difference <= 100) {
+                equalCounter++;
+            } else {
+                equalCounter = 0;
+            }
         }
-
+        
         if (equalCounter >= 100) {
             equalCounter = 0;
-            showCam(video);
+            showCam();
         }
         
     }, 100);
